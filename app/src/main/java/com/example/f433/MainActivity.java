5d38124d0811486a42967c9ce19e5f090cc64d5a
messengerW <messenger232624@gmail.com>
2018-12-06 20:30:10 +0800

    use svg

--- app/src/main/java/com/example/f433/MainActivity.java
+++ app/src/main/java/com/example/f433/MainActivity.java
@@ -141,17 +141,19 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_1) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_2) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_3) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_4) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_5) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_6) {
+
+        } else if (id == R.id.nav_7) {
 
         }
 
