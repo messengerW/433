979f65d3c1e6349a962a011eb1ca1d55d41bf5fe
messengerW <messenger232624@gmail.com>
2018-12-11 22:56:25 +0800

    图片轮播、下滑栏

--- app/src/main/java/com/example/f433/Fragment1.java
+++ app/src/main/java/com/example/f433/Fragment1.java
@@ -2,16 +2,163 @@ package com.example.f433;
 
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.app.Fragment;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Fragment1 extends Fragment {
+
+    private View mView;
+    private ViewPager mViewPaper;
+    private List<ImageView> images;
+    private List<View> dots;
+    private int currentItem;
+    //记录上一次点的位置
+    private int oldPosition = 0;
+    //存放图片的id
+    private int[] imageIds = new int[]{
+            R.mipmap.lunbo_1,
+            R.mipmap.lunbo_2,
+            R.mipmap.lunbo_3
+    };
+
+    private ViewPagerAdapter adapter;
+    private ScheduledExecutorService scheduledExecutorService;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.page1, container, false);
-        return view;
+        mView = inflater.inflate(R.layout.page1, container, false);
+        setView();
+        return mView;
     }
+
+    private void setView() {
+        mViewPaper = (ViewPager) mView.findViewById(R.id.vp);
+
+        //显示图片
+        images = new ArrayList<>();
+        for (int i = 0; i < imageIds.length; i++) {
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setBackgroundResource(imageIds[i]);
+            images.add(imageView);
+        }
+
+        //显示小白点
+        dots = new ArrayList<>();
+        dots.add(mView.findViewById(R.id.dot_0));
+        dots.add(mView.findViewById(R.id.dot_1));
+        dots.add(mView.findViewById(R.id.dot_2));
+
+        adapter = new ViewPagerAdapter();
+        mViewPaper.setAdapter(adapter);
+
+        mViewPaper.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                dots.get(position).setBackgroundResource(R.drawable.ic_point1);
+                dots.get(position).setBackgroundResource(R.drawable.ic_point2);
+
+                oldPosition = position;
+                currentItem = position;
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+    }
+
+    /*定义适配器*/
+    public class ViewPagerAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return images.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0 == arg1;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup view, int position, Object object) {
+            view.removeView(images.get(position));
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup view, int position) {
+            view.addView(images.get(position));
+            return images.get(position);
+        }
+    }
+
+    //利用线性池执行动画轮播
+
+    @Override
+    public void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        scheduledExecutorService.scheduleWithFixedDelay(
+                new ViewPageTask(),
+                2,
+                2,
+                TimeUnit.SECONDS);
+    }
+
+
+    //图片轮播任务
+    private class ViewPageTask implements Runnable {
+        @Override
+        public void run() {
+            currentItem = (currentItem + 1) % imageIds.length;
+            mHandler.sendEmptyMessage(0);
+
+        }
+    }
+
+    //接受子线程传递过来的数据
+
+    private Handler mHandler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            mViewPaper.setCurrentItem(currentItem);
+        }
+
+        ;
+
+    };
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (scheduledExecutorService != null) {
+            scheduledExecutorService.shutdown();
+            scheduledExecutorService = null;
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/f433/MainActivity.java
+++ app/src/main/java/com/example/f433/MainActivity.java
@@ -52,7 +52,6 @@ public class MainActivity extends AppCompatActivity
             return false;
         }
 
-
     };
 
     /**
