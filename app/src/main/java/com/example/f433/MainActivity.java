679d37b76edff7bc40bf76de6973e63519ec9885
messengerW <messenger232624@gmail.com>
2019-03-10 21:00:05 +0800

    整理上学期文档，增加注释

--- app/src/main/java/com/example/f433/MainActivity.java
+++ app/src/main/java/com/example/f433/MainActivity.java
@@ -85,7 +85,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.mainpage);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
