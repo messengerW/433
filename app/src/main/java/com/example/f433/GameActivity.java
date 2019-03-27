5856d1c6b26b1028617a150edeb01985433c4ebd
messengerW <messenger232624@gmail.com>
2019-03-27 23:46:24 +0800

    Revert "Revert "Merge pull request #5 from Lethe1999/master""
    This reverts commit 138cd32ce08bc0e78261aa84d31932394c0143f0.

--- app/src/main/java/com/example/f433/GameActivity.java
+++ app/src/main/java/com/example/f433/GameActivity.java
@@ -5,8 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-import java.util.Random;
-
 public class GameActivity extends AppCompatActivity {
 
     @Override
@@ -14,15 +12,44 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        final GameActivity_ScoreBoardView myView=(GameActivity_ScoreBoardView)findViewById(R.id.custom_view);
-        myView.setOnClickListener(new View.OnClickListener(){
+        //主队积分
+        final GameActivity_ScoreBoardView myView = (GameActivity_ScoreBoardView) findViewById(R.id.custom_view);
+        //客队积分
+        final GameActivity_ScoreBoardView myView2 = (GameActivity_ScoreBoardView) findViewById(R.id.custom_view2);
+        myView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                myView.setColor(Color.rgb(102,204,255));
+            public void onClick(View v) {
+                myView.setColor(Color.rgb(102, 204, 255)); //waterBlue
                 myView.setScore(34);
-                myView.setWinDrawLose(10,4,5);
+                myView.setWinDrawLose(10, 4, 5);
+
+                myView2.setColor(Color.rgb(0, 255, 0));
+                myView2.setScore(31);
+                myView2.setWinDrawLose(8, 7, 4);
             }
-    });
+        });
+        myView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myView.setColor(Color.rgb(102, 204, 255)); //waterBlue
+                myView.setScore(34);
+                myView.setWinDrawLose(10, 4, 5);
+
+                myView2.setColor(Color.rgb(0, 255, 0));
+                myView2.setScore(31);
+                myView2.setWinDrawLose(8, 7, 4);
+            }
+        });
+
+        //主客队联赛中排名
+        final GameActivity_RankBar bar = (GameActivity_RankBar) findViewById(R.id.rank_bar);
+        bar.setRanks(1, 1);
+        bar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bar.setRanks(6, 10);
+            }
+        });
     }
 
 }
